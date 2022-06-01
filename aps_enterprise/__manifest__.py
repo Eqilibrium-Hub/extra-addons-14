5795f5dce039846dece4012c4f467b75eebd605d
Mario Solis <56165442+msolis6686@users.noreply.github.com>
2022-05-31 21:13:56 -0300

    add

---
+++ aps_enterprise/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright (C) 2021 Adrian Paesani.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Odoo EE",
+    "version": "13.0",
+    "author": "Adrian Paesani",
+    "license": "AGPL-3",
+    "category": "base",
+    "depends": [
+        'mail',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
